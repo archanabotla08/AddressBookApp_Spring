5f1e5ecf453b72983ea195b5c95a12c3e8eca4cc
archanabotla08 <archana.botla@gmail.com>
2021-01-09 04:45:38 +0530

    [Archana] Added: UC10 to validate the name and address field with not null and valid characters n length

--- src/main/java/com/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/addressbookapp/dto/AddressBookDTO.java
@@ -1,7 +1,16 @@
 package com.addressbookapp.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class AddressBookDTO {
+	
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Invalid Name")
+	@NotEmpty(message="Should not be null")
 	public String name;
+	
+	@Pattern(regexp = "^[A-Za-z,.0-9]{3,}$",message = "Invalid Address")
+	@NotEmpty(message = "Should not be null")
 	public String address;
 	public AddressBookDTO(String name, String address) {
 		super();
