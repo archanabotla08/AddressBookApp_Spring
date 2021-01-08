1411c2208c1a3bd6edbe63aabe23a0a470f689c0
archanabotla08 <archana.botla@gmail.com>
2021-01-09 04:38:25 +0530

    [Archana] Added: UC9 to set path for database

--- src/main/java/com/addressbookapp/AddressBookAppSpringApplication.java
+++ src/main/java/com/addressbookapp/AddressBookAppSpringApplication.java
@@ -3,14 +3,20 @@ package com.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @ComponentScan("com.addressbookapp")
 public class AddressBookAppSpringApplication {
-
+	private static final org.slf4j.Logger log = 
+		    org.slf4j.LoggerFactory.getLogger(AddressBookAppSpringApplication.class);
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address book app");
-		SpringApplication.run(AddressBookAppSpringApplication.class, args);
+//		SpringApplication.run(AddressBookAppSpringApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppSpringApplication.class, args);
+		log.info("Address Book Started In App");
+		log.info("Address Book Database : ",context.getEnvironment().getProperty("spring.datasource.username"));
+		
 	}
 
 }
