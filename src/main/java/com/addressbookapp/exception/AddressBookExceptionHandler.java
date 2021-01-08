0db2223995f557e45dd21f7eaa7ba5103cd0eb1d
archanabotla08 <archana.botla@gmail.com>
2021-01-09 05:02:25 +0530

    [Archana] Added: UC11 to givin an uerror reponse

---
+++ src/main/java/com/addressbookapp/exception/AddressBookExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.addressbookapp.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.addressbookapp.dto.ResponseDTO;
+
+@ControllerAdvice
+public class AddressBookExceptionHandler extends Exception{
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMessage = errorList.stream().map(mapper -> mapper.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing rest request", errMessage);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+
+}
